6315dd3aaf0fc240ab226cf1480a9cbf791a8e70
Ali-Mostaghimi <52744015+Ali-Mostaghimi@users.noreply.github.com>
2021-01-29 23:16:02 +0330

    Creating adapter and viewHolder for news recyclerView.

---
+++ app/src/main/java/com/alimostaghimi/newsapp/ui/newslist/adapter/NewsListAdapter.kt
@@ -0,0 +1,47 @@
+package com.alimostaghimi.newsapp.ui.newslist.adapter
+
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.recyclerview.widget.RecyclerView
+import com.alimostaghimi.newsapp.R
+import com.alimostaghimi.newsapp.data.model.News
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.item_layout.view.*
+
+class NewsListAdapter(private val newsList: ArrayList<News>) : RecyclerView.Adapter<NewsListAdapter.DataViewHolder>() {
+
+    class DataViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bind(news: News) {
+            itemView.textViewTitle.text = news.title
+            itemView.textViewDescription.text = news.description
+            itemView.textViewSource.text = news.source
+            Glide.with(itemView.imageViewBanner.context)
+                    .load(news.imageUrl)
+                    .into(itemView.imageViewBanner)
+            itemView.setOnClickListener {
+                val builder = CustomTabsIntent.Builder()
+                val costumeTabsIntent = builder.build()
+                costumeTabsIntent.launchUrl(it.context, Uri.parse(news.url))
+
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataViewHolder {
+        return DataViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_layout, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: DataViewHolder, position: Int) {
+        holder.bind(newsList[position])
+    }
+
+    override fun getItemCount(): Int = newsList.size
+
+    fun addData(list: List<News>){
+        newsList.addAll(list)
+    }
+}
