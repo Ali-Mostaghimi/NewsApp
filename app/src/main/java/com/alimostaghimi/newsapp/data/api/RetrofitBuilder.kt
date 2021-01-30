44e46e61a4f365fde27cc2ffb0c464f5a99f82f3
Ali-Mostaghimi <52744015+Ali-Mostaghimi@users.noreply.github.com>
2021-01-30 10:38:27 +0330

    Making apiService accessible in RetrofitBuilder.kt

--- app/src/main/java/com/alimostaghimi/newsapp/data/api/RetrofitBuilder.kt
+++ app/src/main/java/com/alimostaghimi/newsapp/data/api/RetrofitBuilder.kt
@@ -6,10 +6,12 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitBuilder {
     private const val BASE_URL = "https://demo9376629.mockable.io/"
 
-    fun getRetrofit(): Retrofit {
+    private fun getRetrofit(): Retrofit {
         return Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
     }
+
+    val apiService: ApiService = getRetrofit().create(ApiService::class.java)
 }
